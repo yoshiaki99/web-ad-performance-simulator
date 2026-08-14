7aa7496123c44aaf15e236e494bed8d73996b6ce
yoshiaki99 <yoshiaki99@gmail.com>
2026-08-14 14:15:56 +0900

    Fix GitHub Pages asset paths

--- prototype/vite.config.mjs
+++ prototype/vite.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: process.env.GITHUB_PAGES === "true" ? "/web-ad-performance-simulator/" : "/",
   build: {
     outDir: "dist/client",
   },
